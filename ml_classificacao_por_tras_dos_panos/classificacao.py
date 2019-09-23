3ae60a982534a5acfad0761acdcf7aa79c3e820b
Elias Bernardo <ebmm01@gmail.com>
2019-09-23 19:25:47 -0300

    Adicionando cálculo de taxa de acerto - classificador

--- ml_classificacao_por_tras_dos_panos/classificacao.py
+++ ml_classificacao_por_tras_dos_panos/classificacao.py
@@ -31,6 +31,39 @@ print(modelo.predict(misterioso)) # => [-1], ou seja, cachorro.
 
 # podemos também testar vários elementos ao mesmo tempo:
 misterioso2 = [1,0,0]
-teste = [misterioso[0], misterioso2]
+misterioso3 = [0,0,1]
+testes = [misterioso[0], misterioso2, misterioso3]
 
-print(modelo.predict(teste))
+print(modelo.predict(testes))
+
+# Porém eu sei qual animal é cada um 'misterioso':
+# -1 = porco, 1 = cachorro
+marcacoes_teste = [-1, 1, -1]
+
+resultado = modelo.predict(testes)
+print(resultado) # => [-1 1 -1], ou seja, ele 'acertou'
+
+"""
+ Como sabíamos o que cada animal era, podemos fazer uma comparação do resultado real
+ Com o resultado do estimador. onde for 0 quer dizer que o estimador acertou, e qualquer coisa 
+ diferente disso quer dizer que ele errou
+"""
+diferencas = resultado - marcacoes_teste # => [0 0 0], ou seja, ele acertou todos
+print("Diferenças:" ,diferencas)
+
+# Podemos também adicionar o total de acertos numa array:
+acertos = [d for d in diferencas if d==0]
+
+# E o total de acertos é o tamanho desse array:
+total_de_acertos = len(acertos)
+print("Total de acertos:",acertos)
+
+# E verificamos o total de elementos:
+total_de_elementos = len(testes)
+print("Total de elementos de teste:", total_de_elementos)
+
+# Definimos então nossa taxa de acerto:
+
+taxa_de_acerto = 100.0 * (total_de_acertos / total_de_elementos)
+
+print("Taxa de acerto:", taxa_de_acerto, "%")
