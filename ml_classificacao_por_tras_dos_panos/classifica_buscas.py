f47bd7c430310d369c870aa0a79b13c6796464a6
Elias Bernardo <ebmm01@gmail.com>
2019-09-26 21:51:01 -0300

    Finalizando curso alura e adicionando relatório

--- ml_classificacao_por_tras_dos_panos/classifica_buscas.py
+++ ml_classificacao_por_tras_dos_panos/classifica_buscas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.naive_bayes import MultinomialNB 
+from sklearn.ensemble import AdaBoostClassifier
+from collections import Counter
 
 # Leio com o pandas e retono um dataframe
 df = pd.read_csv('buscas.csv')
@@ -17,33 +19,108 @@ xdummies_df = pd.get_dummies(x_df)
 x = xdummies_df.values
 y = y_df.values
 
-# Treino 90% dos valores:
-tamanho_de_treino = int(0.9 * len(y))
-treino_dados = x[:tamanho_de_treino]
-treino_marcacoes = y[:tamanho_de_treino]
+# Treino x% dos valores, a ser definido conforme eu achar melhor:
+porcentagem_de_treino = 0.8
 
-# E os 10% restantes serão usados para testes:
-tamanho_de_teste = int(len(y) - tamanho_de_treino)
-teste_dados = x[-tamanho_de_teste:]
-teste_marcacoes = y[-tamanho_de_teste:]
+tamanho_de_treino = int(porcentagem_de_treino * len(y))
+treino_dados = x[0:tamanho_de_treino]
+treino_marcacoes = y[0:tamanho_de_treino]
+
+# E os y% restantes serão usados para testes:
+porcentagem_de_teste = 0.1
+
+
+"""
+Aqui uma observação: apersar de as variáveis de validação estarem ali em baixo, elas não foram
+adicionadas de cara. Ao contrário, elas foram adicionadas somente ao final, para que todas as verificações sejam de fato
+validadas.
+"""
+tamanho_de_teste = porcentagem_de_teste * len(y)
+tamanho_de_validacao = len(y) - tamanho_de_treino - tamanho_de_teste
+fim_de_teste = int(tamanho_de_treino +tamanho_de_teste)
+
+teste_dados = x[tamanho_de_treino:fim_de_teste]
+teste_marcacoes = y[tamanho_de_treino:fim_de_teste]
+
+validacao_dados = x[fim_de_teste:]
+validacao_marcacoes = y[fim_de_teste:]
+
+
 
 # Crio o modelo
-modelo = MultinomialNB()
+modelo_MultinomialNB = MultinomialNB()
 
-# Treino o modelo
-modelo.fit(treino_dados, treino_marcacoes)
+def treina_e_prediz(teste_marcacoes, teste_dados,treino_dados, treino_marcacoes, modelo):
 
-# Predição usando a base de testes
-resultado = modelo.predict(teste_dados)
+    # Treino o modelo
+    modelo.fit(treino_dados, treino_marcacoes)
+    
+    # Predição usando a base de testes
+    resultado = modelo.predict(teste_dados)
 
-# Agora verificamos a taxa de acerto:
-diferencas = resultado - teste_marcacoes
+    # Agora verificamos a taxa de acerto:
+    acertos = resultado == teste_marcacoes
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(teste_dados)
+    taxa_de_acerto_local = 100.0 * (total_de_acertos/total_de_elementos)
 
-acertos = [d for d in diferencas if d == 0]
-total_de_acertos = len(acertos)
-total_de_elementos = len(teste_dados)
-taxa_de_acerto = 100.0 * (total_de_acertos/total_de_elementos)
+    # treino com 90, testo com 9
+    print("Total de elementos de teste:", total_de_elementos)
+    print(f"Taxa de acerto: {taxa_de_acerto_local}%")
+    return taxa_de_acerto_local
 
-# treino com 90, testo com 9
-print("Total de elementos de teste:", total_de_elementos)
-print("Taxa de acerto:", taxa_de_acerto)
+print("Teste com MultinomialNB: \n")
+resultado_MultinomialNB = treina_e_prediz(teste_marcacoes, teste_dados,treino_dados, treino_marcacoes, modelo_MultinomialNB)
+
+
+"""
+Tive como resultado 82% de acerto. A princípio parece ser um bom número, mas preciso 
+comparar com algo para poder dizer se de fato o é. E como fazer isso? É preciso estimar 
+um 'caso base' com um algoritmo 'burro', ou chutar tudo com um único valor
+"""
+
+
+#Então tentaremos com outro algoritmo: AdaBoostClassifier()
+print("\nTeste com AdaBoostClassifier")
+modelo_AdaBoost = AdaBoostClassifier()
+
+resultado_AdaBoost = treina_e_prediz(teste_marcacoes, teste_dados, treino_dados, treino_marcacoes, modelo_AdaBoost)
+
+"""
+Comparamos qual dos dois algoritmos teve o melhor resultado e a partir disso fazemos uma nota predição
+com os % restante de validação (10% no nosso caso).
+"""
+
+if resultado_MultinomialNB > resultado_AdaBoost:
+    print("\nMultinomialNB teve uma performance melhor.")
+    vencedor = modelo_MultinomialNB
+else:
+    print("\nAdaBoost teve um resultado melhor.")
+    vencedor = modelo_AdaBoost
+
+# E defino uma função que faz a predição com a base se validação
+def teste_real(modelo, validacao_dados, validacao_marcacoes):
+    resultado = vencedor.predict(validacao_dados)
+    acertos = (resultado == validacao_marcacoes)
+
+    total_de_acertos = sum(acertos)
+    total_de_elementos = len(validacao_marcacoes)
+    taxa_de_acerto = 100.0 * total_de_acertos/total_de_elementos
+
+    print(f"Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {taxa_de_acerto}")
+
+teste_real(vencedor, validacao_dados, validacao_marcacoes)
+
+"""
+E fazemos uma comparação com o nosso 'algoritmo burro', que apenas chuta. A partir disso podemos fazer uma comparação com o nosso
+modelo utilizado, e afirmar se ele é bom ou ruim (claro, depende de outros fatores também).
+Vale uma observação aqui: inicialmente calculávamos essa taxa de acerto base com as marcaçes de testes, porém optamos por fazer isso
+com as marcações de validações para validar o nosso algoritmo final. Ou seja, a taxa de acerto do agoritmo vencedor e a taxa de acerto do
+algoritmo burro foram feitas com base nos exatos mesmos dados. 
+"""
+
+# Com o counter eu conto os valores de 0's e 1's, sim e nãp, etc, e pego esses valores com o values()
+acerto_base = max(Counter(validacao_marcacoes).values())
+
+print(f"Taxa de acerto base (algoritmo 'burro'): {acerto_base/len(validacao_marcacoes)*100}%") 
+
--- ml_classificacao_por_tras_dos_panos/relatorio.md
+++ ml_classificacao_por_tras_dos_panos/relatorio.md
@@ -1,13 +1,14 @@
-Inicialmente criamos 6 'seres': 3 cachorros e 3 porcos, com as seguintes características: É gordo? Tem pernas curtas? Late? 
-Para cada ser, já sabíamos tais características e se ele era ou não um cachorro ou porco. A partir disso criamos um indivíduo misterioso, com certas características mas de origem desconhecida (não sabíamos se era um cachorro ou porco) e usamos o algorítmo MultinomialNB para treinar nossa base de dados e classificar o indivíduo misterioso
+## Introdução
 
-## MultinomialNB
+O curso teve por objetivo dar uma introdução sobre Machine Learning e explicar um pouco sobre seu funcionamento. São feitos 3 códigos de exemplo: primeiramente a classificação de animais em cachorros ou porcos, de forma bem simples (sem ao menos um dataset), depois um código para verificar se um usuário que visitou um site sob certos aspectos (se visitou a home, fale conosco, etc) e a partir disso compraria ou não um produto (com um dataset de 100 itens) e por fim um algoritmo muito parecido com o anterior, mas que possuia um dataset maior (cerca de 1000 itens) com variáveis categóricas (qual linguagem de programação uma pessoa x pesquisou). Em todos os programas foi utilizado o algoritmo __MultinomialNB__, e no último em específico também foi utilizado o algoritmo __AdaBoostClassifier__. 
 
-## Taxa de acerto
 
-## Variáveis categóricas 
+## MultinomialNB (Naive Bayes)
 
-Uma variável categórica é uma variável estatística, medida em uma escala nominal, cujas categorias identifiquem a sociedade da classe ou de grupo. O Gênero seria um exemplo de uma variável categórica, com as duas classes ou grupos (masculino e feminino).
+Segundo a documentação: _O classificador __multinomial Naive Bayes__ é adequado para classificação com recursos discretos (por exemplo, contagem de palavras para classificação de texto). _. Ele possui algumas regras de decisão, como __maximum a posteriori__ ou um __pseudo chute__ baseado nas probabilidades de eventos.
 
-Java, ruby , algoritmos => 0,1,0
-Ou seja, para cada  categoria eu crio uma coluna binária cuja resposta é apenas sim ou não (1 ou 0)
+Para o MultinomialNB, fazemos uso de __variáveis categóricas__ , ou seja, _características que não possuem valores quantitativos, mas, ao contrário, são definidas por várias categorias_. Em nosso caso temos variáveis nominais, pois são sempre dados binários. Caso tenhamos dados que não sejam 0's ou 1's, temos que retirar os __dummies__ dessas variáveis (igual fizemos no classificador 3).
+
+## Algoritmo base (algoritmo burro) e taxa de acerto
+
+Por fim, conseguimos estimar uma taxa de acerto para nossos classificadores. Mas há um porém: não há nenhuma outra métrica para dizer se ele está bom ou não. Podemos então chutar todos os valores como 0 ou 1, e pegar o maior valor de acerto desse chute. Isso é chamado algoritmo base, que é basicamente um chute a esmo. Caso esse algoritmo base tenha uma taxa de acerto superior aos nossos classificadores, isso quer dizer que nosso algoritmo perde para um simples chute, então não está bom. Contudo não se pode automaticamente inferir o contrário, ou seja, que se nosso classificador for superior ao algoritmo base ele é bom. Isso se dá principalemente pelo fato de que a porcentagem de acerto deve considerar diversos fatores como quantidade de dados testada (e/ou validada), o problema que estou trabalhando (prevendo se um usuário fará uma compra ou a chance de haver um terremoto?) ou mesmo questões negociais. Dessa forma o algoritmo base serve como uma estimativa inicial, e não final.
